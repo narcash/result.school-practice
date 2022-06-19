88a16b7350d21c76c8971e753ce7663a99a1152c
Nariman <mirmanov1@mail.ru>
2022-06-19 22:52:32 +0300

    feat: first commit, add home-branch

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// 1. Найти сумму чисел от 0 для 100000
+
+// let sum = 0;
+// for(i = 0; i <=100000; i++) {
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+//2.  Найти сумму элементов массива
+
+
+// let cells = [100, 50, 20, 80, 30, 10, 60]
+// for(let i = 0; i < cells.length; i++){
+//     console.log(cells[i])
+// }
+// let sumArray = 0;
+// for(let i = 0; i < cells.length; i++) {
+//     sumArray = sumArray + cells[i];
+//     console.log(sumArray)
+// }
